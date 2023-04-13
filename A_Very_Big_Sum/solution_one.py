cac033776f40750204b502ad5235b21cbd3b1040
jweel <jweelkar2016@gmail.com>
2023-04-13 22:01:07 +0600

    Add some solutions of A Very Big Sum problem.

---
+++ A_Very_Big_Sum/solution_one.py
@@ -0,0 +1,5 @@
+def aVeryBigSum(ar):
+    # Write your code here
+    
+    res = sum(ar)
+    return res
---
+++ A_Very_Big_Sum/solution_three.py
@@ -0,0 +1,9 @@
+def aVeryBigSum(ar):
+    # Write your code here
+    
+    total = 0
+    
+    for i in range(len(ar)):
+        total += ar[i]
+        
+    return total
